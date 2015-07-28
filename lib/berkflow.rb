96635c07745350c82c740c93b0dff9213298b6fe
Jamie Winsor <jamie@vialstudios.com>
2015-07-28 14:38:23 -0700

    disable Celluloid logger

--- lib/berkflow.rb
+++ lib/berkflow.rb
@@ -4,8 +4,11 @@ require 'ridley'
 require 'ridley-connectors'
 require 'berkshelf'
 
-Celluloid.logger.level = ::Logger::ERROR
-
 module Berkflow
   # Your code goes here...
 end
+
+# Disable Celluloid logging to silence erronous warnings and errors. This
+# will be re-enabled in the future once the cause of false crash reports
+# on shutdown is identified and fixed.
+Celluloid.logger = nil
