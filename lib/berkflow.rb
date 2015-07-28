584991be62918673e2ee1933a3f3f835e614a694
Jamie Winsor <jamie@vialstudios.com>
2015-07-28 14:43:28 -0700

    configure app on startup
    use ERROR level for logger by default
    if DEBUG env variable is set then use DEBUG level for logger

--- lib/berkflow.rb
+++ lib/berkflow.rb
@@ -5,10 +5,26 @@ require 'ridley-connectors'
 require 'berkshelf'
 
 module Berkflow
-  # Your code goes here...
+  class << self
+    def debug?
+      !ENV['DEBUG'].nil?
+    end
+
+    def setup
+      if debug?
+        Ridley.logger.level    = Logger::DEBUG
+        Berkshelf.logger.level = Logger::DEBUG
+      else
+        # Disable Celluloid logging to silence erronous warnings and errors. This
+        # will be re-enabled in the future once the cause of false crash reports
+        # on shutdown is identified and fixed.
+        Celluloid.logger = nil
+
+        Ridley.logger.level    = Logger::ERROR
+        Berkshelf.logger.level = Logger::ERROR
+      end
+    end
+  end
 end
 
-# Disable Celluloid logging to silence erronous warnings and errors. This
-# will be re-enabled in the future once the cause of false crash reports
-# on shutdown is identified and fixed.
-Celluloid.logger = nil
+Berkflow.setup
